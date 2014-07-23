7330dcaabb5ab10afde51a0e320dda93686292c1
Sam Haycraft <phreakdawg@gmail.com>
2014-07-23 16:29:23 -0600

    updating database and showing article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,10 +3,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[article_params])
+    @article = Article.new(article_params)
     
     @article.save
     redirect_to @article
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
